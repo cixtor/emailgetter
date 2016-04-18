091b18a6f2f74e3dd25026692f77704a542c92e4
Cixtor <cixtords@gmail.com>
2016-04-18 09:00:00 -0500

    Added generic function to send HTTP GET requests
    The function adds functionality to the program to send HTTP GET requests
    with some custom headers including the user-agent and the accept which
    will be required to retrieve the data from GitHub's API service, the
    other headers are not necessary but I will keep them to simulate that
    this is a web browser, although this can be easily detected in the host
    by measuring the time between packets

--- emailgetter.go
+++ emailgetter.go
@@ -3,10 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 var username = flag.String("u", "", "Username to query")
+
+func httpGetRequest(url string) []byte {
+	client := http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	req.Header.Set("DNT", "1")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.8")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (KHTML, like Gecko) Safari/537.36")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	// I understand that ioutil.ReadAll is bad.
+	content, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return content
+}
 
 func main() {
 	flag.Parse()
