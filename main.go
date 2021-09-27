d8425c2c364c876613e9d4d8325e50ab02984ec7
Cixtor <cixtords@gmail.com>
2021-09-27 10:34:28 -0800

    Modify program options to simplify the query syntax

--- main.go
+++ main.go
@@ -3,49 +3,51 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"sync"
 )
 
-var username = flag.String("username", "", "Username to execute the query")
-var followers = flag.Bool("followers", false, "Get emails of follower users")
-var following = flag.Bool("following", false, "Get emails of following users")
-var noemails = flag.Bool("noemails", false, "Get the usernames instead of the emails")
-var page = flag.Int("page", 1, "Page number for following and followers")
+var followers bool
+var following bool
+var onlyUsernames bool
+var pageNumber int
 
 func main() {
+	flag.BoolVar(&followers, "followers", false, "Print the email(s) associated to each follower")
+	flag.BoolVar(&following, "following", false, "Print the email(s) associated to each following")
+	flag.BoolVar(&onlyUsernames, "only-usernames", false, "Print following/followers usernames instead of emails")
+	flag.IntVar(&pageNumber, "page", 1, "Page number for following and followers")
+
 	flag.Usage = func() {
-		fmt.Println("E-Mail Getter")
-		fmt.Println("  http://cixtor.com/")
-		fmt.Println("  https://github.com/cixtor/emailgetter")
-		fmt.Println("  https://en.wikipedia.org/wiki/Email_address_harvesting")
-		fmt.Println("Usage:")
+		fmt.Println("usage: emailgetter [OPTIONS] [USERNAME]")
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
 
-	if *username == "" {
-		fmt.Println("Missing username to query")
+	username := flag.Arg(0)
+
+	if username == "" {
 		flag.Usage()
-		return
+		os.Exit(2)
 	}
 
 	var wg sync.WaitGroup
 	var getter EmailGetter
 
-	getter.PageNumber = *page
+	getter.PageNumber = pageNumber
 
-	if *noemails {
+	if onlyUsernames {
 		getter.OnlyUsers = true
 	}
 
 	wg.Add(1) /* At least wait for one */
-	go getter.RetrieveEmail(&wg, *username)
+	go getter.RetrieveEmail(&wg, username)
 
-	if *following {
-		getter.RetrieveFollowing(&wg, *username)
-	} else if *followers {
-		getter.RetrieveFollowers(&wg, *username)
+	if following {
+		getter.RetrieveFollowing(&wg, username)
+	} else if followers {
+		getter.RetrieveFollowers(&wg, username)
 	}
 
 	wg.Wait()
