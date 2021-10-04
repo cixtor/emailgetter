e4f0fa0b69747c9538a73f0b6d4e24a3fe2cabc6
Cixtor <cixtords@gmail.com>
2021-10-04 10:34:28 -0800

    Add option to control the number of concurrent HTTP requests

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ var followers bool
 var following bool
 var onlyUsernames bool
 var pageNumber int
+var maxThreads int
 
 func main() {
 	flag.BoolVar(&debugMode, "debug", false, "Print the URLs that are being requested")
 	flag.BoolVar(&followers, "followers", false, "Print the email(s) associated to each follower")
 	flag.BoolVar(&following, "following", false, "Print the email(s) associated to each following")
 	flag.BoolVar(&onlyUsernames, "only-usernames", false, "Print following/followers usernames instead of emails")
-	flag.IntVar(&pageNumber, "page", 1, "Page number for following and followers")
+	flag.IntVar(&pageNumber, "p", 1, "Page number for following and followers")
+	flag.IntVar(&maxThreads, "t", 10, "Maximum number of concurrent HTTP requests")
 
 	flag.Usage = func() {
 		fmt.Println("usage: emailgetter [OPTIONS] [USERNAME]")
@@ -33,14 +35,11 @@ func main() {
 		os.Exit(2)
 	}
 
-	getter := NewEmailGetter(20)
+	getter := NewEmailGetter(maxThreads)
 
 	getter.DebugMode = debugMode
 	getter.PageNumber = pageNumber
-
-	if onlyUsernames {
-		getter.OnlyUsers = true
-	}
+	getter.OnlyUsers = onlyUsernames
 
 	getter.RetrieveEmail(username)
 
